ebaf4feb52ff3ea25d90b15b8a57cdb94e5eb3a3
hamidsalemi77 <48052326+hamidsalemi77@users.noreply.github.com>
2020-05-19 09:24:32 +0430

    allfile

---
+++ 32bregen.sv
@@ -0,0 +1,14 @@
+`timescale 1ns/1ns
+
+module b32regen(input [31:0] in, input clk, rst, enable, output reg [31:0] out);
+  always@(posedge clk, posedge rst) begin
+    if(rst)
+      out = 31'b0;
+    else 
+        if(enable)
+          out <= in;
+    end
+endmodule
+
+
+
